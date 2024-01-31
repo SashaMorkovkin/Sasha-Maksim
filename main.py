fe8e9746607a8e3c8ed0e1bd5b2494b3f98bce1a
guspatrul <123506705+guspatrul@users.noreply.github.com>
2024-01-31 16:26:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -36,3 +36,53 @@ if __name__ == '__main__':
     ex = MyWidget()
     ex.show()
     sys.exit(app.exec_())
+
+
+import os
+import sys
+import requests
+import pprint
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.get_image()
+        self.initUI()
+
+    def get_image(self):
+        request = 'http://static-maps.yandex.ru/1.x/?ll=2.294494,48.858247&spn=0.0000001645799,0.0061999&l=sat'
+        response = requests.get(request)
+        if response:
+            self.map_file = 'map.jpg'
+            with open(self.map_file, 'wb') as file:
+                file.write(response.content)
+
+
+    def initUI(self):
+        self.setGeometry(300, 300, 800, 600)
+        self.setWindowTitle('Карта')
+        self.pixmap = QPixmap(self.map_file)
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(800, 600)
+        self.image.setPixmap(self.pixmap)
+
+    def closeEvent(self, event):
+        os.remove(self.map_file)
+
+    def keyPressEvent(self, event):
+            if event.key() == Qt.Key_Enter:
+                print('Ура')
+                sys.exit()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
+
